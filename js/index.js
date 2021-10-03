64eae18a99b58d549831a3e5c39bfefd811dd5c9
Erik Boye Sørensen <69841837+erikboye@users.noreply.github.com>
2021-10-03 14:14:14 +0200

    123

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// imports always go on top
+import { testLengthOfInput } from "./libs/validation.js";
+import writeToDOM from "./libs/writeToDom.js";
+import {
+  saveToLocalStorage,
+  getStorageItem,
+} from "./libs/localStorageHelper.js";
+
+const itemInput = document.querySelector(".itemInput");
+const addItem = document.querySelector(".addItem");
+const items = document.querySelector(".items");
+let groceryArray = getStorageItem("groceryArrayKey");
+const clearBtn = document.querySelector(".deleteBtn");
+writeToDOM(items, groceryArray);
+
+addItem.onclick = function () {
+  let valueOfGroceryInputBox = itemInput.value;
+  if (testLengthOfInput(valueOfGroceryInputBox, 3)) {
+    let groceryItem = {
+      id: groceryArray.length,
+      name: valueOfGroceryInputBox,
+    };
+    groceryArray.push(groceryItem);
+    saveToLocalStorage("groceryArrayKey", groceryArray);
+    writeToDOM(items, groceryArray);
+  } else {
+    console.log("Input needs more characters");
+  }
+  itemInput.value = "";
+};
+
+clearBtn.onclick = function () {
+  localStorage.clear();
+  groceryArray = [];
+  writeToDOM(items, groceryArray);
+};
